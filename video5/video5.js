6a780e6da7446581e07d30f32516f5e15e5730cb
Quang <vodinhquang0505@gmail.com>
2022-03-17 00:32:28 +0700

    video5-1

--- video5/video5.js
+++ video5/video5.js
@@ -2,3 +2,5 @@
 function Myfuntion() {
     document.querySelector('.video5').innerHTML = document.querySelector('.video5').innerHTML.replace('Quang', 'Đình Quang');
 }
+let a = document.querySelector('#quang').innerHTML;
+document.querySelector('.quang').innerHTML = a.split(' ')
