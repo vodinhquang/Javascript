473ce7d578a0c130adeb430c2ce99ffd6335cf19
Quang <vodinhquang0505@gmail.com>
2022-03-18 00:35:26 +0700

    video6-2

--- video6/video6.js
+++ video6/video6.js
@@ -5,3 +5,6 @@ let d = +b;
 let e = '6';
 let f = Number(e)
 console.log('a is:',typeof a, 'b is:', typeof b, b===c, c===d, f===a);
+
+let text =document.querySelector('#quang').innerHTML //# là id
+document.querySelector('.quang').innerHTML = text.toUpperCase() //. là class
