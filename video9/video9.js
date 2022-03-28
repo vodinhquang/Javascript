3db92825cf09b6a69876c8e1dca0d68cb617bdc9
Quang <vodinhquang0505@gmail.com>
2022-03-29 00:43:46 +0700

    video9-1

--- video9/video9.js
+++ video9/video9.js
@@ -1,2 +1,7 @@
-a = ['Samsung','Iphone','Xiaomi']
+let a = ['Samsung','Iphone','Xiaomi']
 console.log(`a la object or array: `,a)
+console.log('the first item:',a[0])
+let b = a[1]
+console.log(b)
+ 
+document.querySelector('.quang').innerHTML = a.length
