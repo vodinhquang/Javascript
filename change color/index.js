aebe036738f67ed3a30a98c4adb98793a878dce1
Quang <vodinhquang0505@gmail.com>
2022-04-02 00:38:34 +0700

    change color

---
+++ change color/index.js
@@ -0,0 +1,9 @@
+function change(){
+    document.querySelector('.img').src='./trang.jpeg'
+}
+function change1(){
+    document.querySelector('.img').src='./den.jpeg'
+}
+function change2(){
+    document.querySelector('.img').src='./xam.jpeg'
+}
