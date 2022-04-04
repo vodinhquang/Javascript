20eb4f96f62f12db891f089245d500152cbce1f0
Quang <vodinhquang0505@gmail.com>
2022-04-05 00:55:46 +0700

    video11

---
+++ video11/video11.js
@@ -0,0 +1,9 @@
+console.log('Html with JS')
+let a = ['Arsenal', 'Livepool','Chealsea'];
+for (let i = 0; i < a.length; ++i){
+    console.log('Những đội bóng ngoại hạng Anh :',a[i])
+}
+
+let i = 1;
+let j = i++;
+ console.log('i =',i,'j =',j)
