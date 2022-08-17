472c0005289283d053d250387f675233ebec8aa5
vodinhquang <vodinhquang0505@gmail.com>
2022-08-18 00:59:58 +0700

    video17

---
+++ video17/video17.js
@@ -0,0 +1,15 @@
+function sum(a,b,i){
+    console.log('1')
+    console.log('2')
+    console.log('3')
+    if (i === 4){
+        return
+    }
+    console.log('4')
+    console.log('5')
+    console.log('6')
+    return a+b
+
+}
+
+console.log('sum:',sum(4,2,4))
