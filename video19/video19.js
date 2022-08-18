c29c58b66c14487db1b53ab2cd41d89dc32ae2d6
vodinhquang <vodinhquang0505@gmail.com>
2022-08-19 01:11:30 +0700

    video19

---
+++ video19/video19.js
@@ -0,0 +1,15 @@
+//funtion
+let sum = (a,b) => {
+    return a + b
+}
+
+console.log('sum a + b =',sum(9,6))
+
+let obj = {
+    name: 'Quang',
+    age : 28,
+    getName: function (){   
+        return this.name;
+    }
+}
+console.log('my name is:',obj.getName())
