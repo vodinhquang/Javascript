a12e67a7fa9a3c7d528535c2cd19e0f65522093e
vodinhquang <vodinhquang0505@gmail.com>
2022-09-06 10:16:21 +0700

    video22

---
+++ video22/video22.js
@@ -0,0 +1,9 @@
+
+//map tra lai 1 mang moi ko lam thay doi data cua mang goc
+let arr = [0,1,2,3,4,5,6,7,8,9,10]
+let map = arr.map((num)=>{
+    return num * num
+});
+console.log(map)
+console.log(arr)
+
